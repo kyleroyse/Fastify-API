5c96ffe1cbaac7e3fe57ec8e42563a6be86b2c5b
Kyle Royse <kyle.royse@doane.edu>
2019-06-18 16:29:46 -0600

    Added new controllers
    Added new controllers to accommodate for owners and services

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,60 +1,60 @@
-// External Dependencies
-const boom = require('boom');
+// External Dependancies
+const boom = require('boom')
 
-// Get Tada Models
-const Car = require('../models/car');
+// Get Data Models
+const Car = require('../models/Car')
 
 // Get all cars
-exports.getCars = async (req, reply) => {
-    try {
-        const cars = await Car.find();
-        return cars;
-    } catch (err) {
-        throw boom.boomify(err);
-    }
+exports.getCars = async () => {
+	try {
+		const cars = await Car.find()
+		return cars
+	} catch (err) {
+		throw boom.boomify(err)
+	}
 }
 
 // Get single car by ID
-exports.getSingleCar = async (req, reply) => {
-    try {
-        const id = req.params.id;
-        const car = await Car.findById(id);
-        return car;
-    } catch (err) {
-        throw boom.boomify(err);
-    }
+exports.getSingleCar = async req => {
+	try {
+		const id = req.params === undefined ? req.id : req.params.id
+		const car = await Car.findById(id)
+		return car
+	} catch (err) {
+		throw boom.boomify(err)
+	}
 }
 
-// Add a new car 
-exports.addCar = async (req, reply) => {
-    try {
-        const car = new Car(req.body);
-        return car.save();
-    } catch (err) {
-        throw boom.boomify(err);
-    }
+// Add a new car
+exports.addCar = async req => {
+	try {
+		const car = new Car(req)
+		const newCar = await car.save()
+		return newCar
+	} catch (err) {
+		throw boom.boomify(err)
+	}
 }
 
 // Update an existing car
-exports.updateCar = async (req, reply) => {
-    try {
-        const id = req.params.id;
-        const car = req.body;
-        const { ...updateData } = car;
-        const update = await Car.findByIdAndUpdate(id, updateData, { new: true });
-        return update;
-    } catch (err) {
-        throw boom.boomify(err);
-    }
+exports.updateCar = async req => {
+	try {
+		const id = req.params === undefined ? req.id : req.params.id
+		const updateData = req.params === undefined ? req : req.params
+		const update = await Car.findByIdAndUpdate(id, updateData, { new: true })
+		return update
+	} catch (err) {
+		throw boom.boomify(err)
+	}
 }
 
 // Delete a car
-exports.deleteCar = async (req, reply) => {
-    try {
-        const id = req.params.id;
-        const car = await Car.findByIdAndRemove(id);
-        return car;
-    } catch (err) {
-        throw boom.boomify(err);
-    }
+exports.deleteCar = async req => {
+	try {
+		const id = req.params === undefined ? req.id : req.params.id
+		const car = await Car.findByIdAndRemove(id)
+		return car
+	} catch (err) {
+		throw boom.boomify(err)
+	}
 }
