df46df967a5809b562da11089e452377b54e032b
Kyle Royse <kyle.royse@doane.edu>
2019-06-18 16:08:01 -0600

    Added new models

---
+++ models/Owner.js
@@ -0,0 +1,10 @@
+// External Dependencies
+const mongoose = require('mongoose');
+
+const ownerSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String
+});
+
+module.exports = mongoose.model('Owner', ownerSchema);
