b0b9e1d62e7149af5c769b118c920c727e09d959
Kyle Royse <kyle.royse@doane.edu>
2019-07-02 20:28:45 -0600

    Import GraphQL and configure GraphQL Schema

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Import Server
-const fastify = require('./server.js');
+const fastify = require('./server.js')
+
+// Import external dependancies
+const gql = require('fastify-gql')
+
+// Import GraphQL Schema
+const schema = require('./schema')
+
+// Register Fastify GraphQL
+fastify.register(gql, {
+   schema,
+   graphiql: true
+})
 
 // Import Routes
 const routes = require('./routes');
